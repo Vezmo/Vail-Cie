6b9474d9647b034a552404a22d95adb3085f7ee0
Samuel Vaillancourt <sam.vail22@gmail.com>
2018-01-05 15:45:03 -0500

    Projectile et Monstre
    GAYY

--- Vail & Cie/Assets/Scripts/Chrono.cs
+++ Vail & Cie/Assets/Scripts/Chrono.cs
@@ -2,76 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chrono : MonoBehaviour {
+public class Chrono : MonoBehaviour
+{
     public int Speed;
     public int LeftDirection;
     public int RightDirection;
     public int UpDirection;
     public int DownDirection;
+    public GameObject Clone;
+    AudioSource AudioSourceGunSound;
+
+
 
 
     Animator animator;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        AudioSourceGunSound = GetComponent<AudioSource>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
     {
-        //Moveleft released
-        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            animator.SetBool("Left", false);
+            AudioSourceGunSound.Play();
         }
 
-        //Moveright released
-        if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            animator.SetBool("Right", false);
-        }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                GameObject bullet = Instantiate(Clone, transform.position, Quaternion.identity) as GameObject;
+                
+            }
+            //Moveleft released
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                animator.SetBool("Left", false);
+            }
 
-        //MoveUp released
-        if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            animator.SetBool("Up", false);
-        }
+            //Moveright released
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                animator.SetBool("Right", false);
+            }
 
-        //Movedown released
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            animator.SetBool("Down", false);
-        }
+            //MoveUp released
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                animator.SetBool("Up", false);
+            }
 
-        //Left Movement
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //Moveleft
-            transform.position = new Vector2(transform.position.x + LeftDirection * Speed * Time.deltaTime, transform.position.y);
-            animator.SetBool("Left", true);
-        }
-        //Right Movement
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //MoveRight
-            transform.position = new Vector2(transform.position.x + RightDirection * Speed * Time.deltaTime, transform.position.y);
-            animator.SetBool("Right", true);
-        }
-        //Up Movement
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            //MoveUp
-            transform.position = new Vector2(transform.position.x , transform.position.y + UpDirection *Speed * Time.deltaTime);
-            animator.SetBool("Up", true);
-        }
-        //Down Movement
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            //MoveDown
-            transform.position = new Vector2(transform.position.x , transform.position.y + DownDirection * Speed * Time.deltaTime);
-            animator.SetBool("Down", true);
+            //Movedown released
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                animator.SetBool("Down", false);
+            }
+
+            //Left Movement
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                //Moveleft
+                transform.position = new Vector2(transform.position.x + LeftDirection * Speed * Time.deltaTime, transform.position.y);
+                animator.SetBool("Left", true);
+            }
+            //Right Movement
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //MoveRight
+                transform.position = new Vector2(transform.position.x + RightDirection * Speed * Time.deltaTime, transform.position.y);
+                animator.SetBool("Right", true);
+            }
+            //Up Movement
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                //MoveUp
+                transform.position = new Vector2(transform.position.x, transform.position.y + UpDirection * Speed * Time.deltaTime);
+                animator.SetBool("Up", true);
+            }
+            //Down Movement
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                //MoveDown
+                transform.position = new Vector2(transform.position.x, transform.position.y + DownDirection * Speed * Time.deltaTime);
+                animator.SetBool("Down", true);
+            }
         }
     }
 }
